72cf0301b7183bbf9d772134a01aab5f07f593e1
Tatraa <tatra5342@gmail.com>
2024-12-12 23:47:00 +0100

    Met Stat 3 zestw

--- sem-1/metody-stat/zestaw1/zestaw1.py
+++ sem-1/metody-stat/zestaw1/zestaw1.py
@@ -33,60 +33,17 @@ class RuinsParam(object):
         
 
 class GameA(RuinsParam):
-    def __init__(self, A, B, a, b, pA, pB, qA, qB):
-        super().__init__(A, B, a, b, pA, pB, qA, qB)
-        self.A = A
-        self.B = B
-        self.a = a
-        self.b = b
-        self.pA = pA
-        self.pB = pB
-        self.qA = qA
-        self.qB = qB
-
-    def game(self):
-        """
-        Funkcja symulująca grę
-        """
-        while self.a > 0 and self.b > 0:
-            if np.random.rand() < self.pA:
-                self.a += 1
-                self.b -= 1
-            else:
-                self.a -= 1
-                self.b += 1
-        return self.a
-
-    def game_sim(self, n):
-        """
-        Funkcja symulująca n gier
-        """
-        results = []
-        for i in range(n):
-            results.append(self.game())
-        return results
-
-    def game_stats(self, n):
-        """
-        Funkcja zwracająca statystyki z n gier
-        """
-        results = self.game_sim(n)
-        return pd.Series(results).describe()
-
-    def game_hist(self, n):
-        """
-        Funkcja rysująca histogram z n gier
-        """
-        results = self.game_sim(n)
-        plt.hist(results, bins=range(min(results), max(results)+1), align='left')
-        plt.xlabel('Kapitał gracza A')
-        plt.ylabel('Liczba gier')
-        plt.title('Histogram kapitału gracza A')
-        plt.savefig('histogram.png')
-        #plt.show()
+    def simulate(self, num_simulations=1000):
+        ruins = 0
+        for _ in range(num_simulations):
+            a, b = self.a, self.b
+            while a > 0 and b > 0:
+                if np.random.rand() < self.pA:
+                    b -= 1
+                else:
+                    a -= 1
+            if a == 0:
+                ruins += 1
+        return ruins / num_simulations
 
 
-if __name__ == '__main__':
-    game = GameA('A', 'B', 20, 20, 0.6, 0.4, 0.4, 0.6)
-    print(game.game_stats(1000))
-    game.game_hist(1000)
